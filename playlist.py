b842780bf8b5f73e5e94995c3e736facba68cb21
demirose41 <demirose41@gmail.com>
2021-07-22 22:54:56 -0700

    1. initialized a one-off excersise to turn incorporate built-in functions

---
+++ playlist.py
@@ -0,0 +1,21 @@
+playlist = {
+    'title': 
+    'SK8T OR DYE', 
+    'author':'ME!!!!!', 
+    'songs': [
+        {'title':'song1','artist':['blue'],'duration': 2.5},
+        {'title':'song2','artist':['kitty','djcat'],'duration': 5.25},
+        {'title':'nyaaah','artist':['chitty'],'duration': 1.25},
+    ]
+}
+
+for song in playlist['songs'] :
+    print(song['duration'])
+    print(song['title'])
+    print(song['artist'])
+    
+
+
+
+
+
